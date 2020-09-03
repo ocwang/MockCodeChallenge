3ac3b34148cb18f05a0f9784236867b1a340a812
Chase Wang <hi@ocwang.com>
2020-09-02 21:42:26 -0400

    Initial Commit

---
+++ CodeChallengeMockup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeChallengeMockup
+//
+//  Created by Chase Wang on 9/2/20.
+//  Copyright © 2020 ocw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
